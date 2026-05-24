7fcdfcd21a44b14b53d16f0f4bdc064b7a44cdb8
for-tristan <marous20st@gmail.com>
2026-05-24 17:35:36 +0300

    delete

--- src/app/dashboard/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const dynamic = 'force-dynamic';
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- src/app/study/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const dynamic = 'force-dynamic';
-
-export default function StudyLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
